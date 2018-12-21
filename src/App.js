8d1bc3c2709f7bc631ced81053196d587b6bab4d
ipandz00 <ipandz00@gmail.com>
2018-12-21 17:22:54 +0100

    Input field added with its handlers and state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import './App.css';
 import ColorContainer from './components/ColorContainer.js';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fieldText: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <ColorContainer />
+          <input type="text" name="fieldText" value={this.state.fieldText} onChange={this.handleInputChange} />
           <a
             className="App-link"
             href="https://reactjs.org"
