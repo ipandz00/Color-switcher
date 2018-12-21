f48e5c5fe0c9227aa1d188b0d0c629a439df92cf
ipandz00 <ipandz00@gmail.com>
2018-12-21 16:24:36 +0100

    Added Color component to App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <ColorContainer />
           <a
             className="App-link"
             href="https://reactjs.org"
