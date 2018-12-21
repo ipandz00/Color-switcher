f1712dcfea9e323bee3c43368aa6407a417e81bc
ipandz00 <ipandz00@gmail.com>
2018-12-21 16:48:38 +0100

    Color fetch added
    Added promise function to return a new color from specified url
    Added helper function to get random integer
    Added logic to save colors into component state

--- src/components/ColorContainer.js
+++ src/components/ColorContainer.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 import Color from '../views/Color.js';
+import { getRndInteger } from '../utils/helper.js';
 
 export default class ColorContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentColor: '000000',
+      colorArray: ['000000']
+    };
+  }
+
+  componentDidMount() {
+    const firstColor = this.getRandomColor();
+    const secondColor = this.getRandomColor();
+    
+    Promise.all([firstColor, secondColor])
+      .then((values) => {
+        let colorArray = this.state.colorArray;
+        let randInt = getRndInteger(0,1);
+        colorArray.push(values[randInt].new_color);
+        this.setState({colorArray: colorArray});
+      });
+  }
+
+  getRandomColor() {
+    return new Promise((resolve, reject) => {
+      fetch('http://www.colr.org/json/color/random')
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error))
+      })
+  }
+
   render() {
     return <Color />
   }
--- src/views/Color.js
+++ src/views/Color.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Color = (props) => {
-	return <p>Switch me! </p>;
+	return <p>Switch me!</p>;
 }
 
 export default Color;
